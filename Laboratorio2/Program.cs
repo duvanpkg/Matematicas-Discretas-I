867a6ebcfd035de39cd32f062f7f380baed08bad
Duvan Díaz <47454957+KillTheReptiles@users.noreply.github.com>
2019-07-26 18:59:19 -0500

    Laboratorio2

---
+++ Laboratorio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("How many test cases do you wanna do?");
+            string input = Console.ReadLine();
+            int t = int.Parse(input);
+
+            for (int i = 1; i < (t+1); i++)
+            {
+                Console.WriteLine("\nThis is the test case number: " + i);
+                Console.WriteLine("\nWrite 2 numbers n and k separated by a comma(,) \nin which n is the number of integers in the list and k is any integer\n ");
+                var nk = Console.ReadLine();
+                var nksplit = nk.Split(','); //separar el input entre comas
+                var n = int.Parse(nksplit[0]); //Primer entero del input (n) 
+                var k = int.Parse(nksplit[1]); //Segundo entero del input (k)
+
+                Console.WriteLine("\nOk, now write a number of n digits (remember you said the value of n)");
+                var n_list = Console.ReadLine();
+            }
+
+        }
+    }
+}
